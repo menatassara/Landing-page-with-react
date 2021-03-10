48f63073796a3b0251d0f6c7deb790e8ab06933f
menatassara <josemt-12-@hotmail.com>
2021-03-10 14:38:22 +0000

    casi 4

--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "React";
 
 const Cards = () => {
@@ -21,30 +20,6 @@ const Cards = () => {
             </div>
         </div>
     );
-=======
-import React from "react";
-
-const Cards = () => {
-	return (
-		<div className="card">
-			<img
-				src="https://a.storyblok.com/f/89778/1168x728/b7082a9dd9/cr_00_00_2.jpeg"
-				className="card-img-top"
-				alt="..."
-			/>
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the cards content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
-			</div>
-		</div>
-	);
->>>>>>> 20f1b05b54febca37efcee6856794d4878fb0f63
 };
 
 export default Cards;
--- src/js/component/CardsC.js
+++ src/js/component/CardsC.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "React";
 import Cards from "./Cards";
 
@@ -22,28 +21,3 @@ const CardsC = () => {
 };
 
 export default CardsC.js;
-=======
-import React from "react";
-import Cards from "./Cards";
-
-const cardsContainer = () => {
-	return (
-		<div className="row">
-			<div className="col-3">
-				<Cards />
-			</div>
-			<div className="col-3">
-				<Cards />
-			</div>
-			<div className="col-3">
-				<Cards />
-			</div>
-			<div className="col-3">
-				<Cards />
-			</div>
-		</div>
-	);
-};
-
-export default cardsContainer;
->>>>>>> 20f1b05b54febca37efcee6856794d4878fb0f63
